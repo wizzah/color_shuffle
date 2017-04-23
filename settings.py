c03185f73d1ba83e61df16736d9def6553aff942
Elizabeth Williams <liz@Elizabeths-MacBook-Pro.local>
2017-04-22 22:19:55 -0400

    initial commit

---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+
+# extracted gif images and processed images become this
+input_filetype = ".gif"
+
+# this usually stays as gif
+output_filetype = ".gif"
+
+directory = os.getcwd()
+directory += "/output"
+
+input_filename = "example_input.gif"
+output_filename = "example_output.gif"
+# got stumped on something, but tried to make a picture anyway
+attempted_output_filename = "itried.gif"
---
+++ stitch.py
@@ -0,0 +1,21 @@
+import imageio
+import os
+import settings
+
+images = []
+sort_names = []
+
+for filename in os.listdir(settings.directory):
+    if filename.endswith(settings.input_filetype):
+        sort_names.append(int(filename[:-(len(settings.input_filetype))]))
+
+# this avoids that filename iteration problem with digits
+sort_names = sorted(sort_names)
+for file in sort_names:
+        try:
+            images.append(imageio.imread(settings.directory+"/"+str(file)+settings.input_filetype))
+        except: 
+            imageio.mimsave(settings.attempted_output_filename, images)
+imageio.mimsave(settings.output_filename, images)
+
+# current issue: https://github.com/imageio/imageio/issues/210
